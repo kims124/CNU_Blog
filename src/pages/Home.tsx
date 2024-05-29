2b89bfcb24bff4a293942f0bc0e3a0dd93bdff6e
kims124 <kyj2157098@gmail.com>
2024-05-29 20:51:19 +0900

    revise_2

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,14 @@
-import PostListItem from '../../../../cse 3-1/실전코딩/CNU_Blog/src/components/PostListItem.tsx';
-import NoPostList from '../../../../cse 3-1/실전코딩/CNU_Blog/src/components/NoPostList.tsx';
-import useGetPostList from '../../../../cse 3-1/실전코딩/CNU_Blog/src/queries/useGetPostList.ts';
+import PostListItem from '../components/PostListItem';
+import NoPostList from '../components/NoPostList';
+import useGetPostList from '../queries/useGetPostList.ts';
 
 const Home = () => {
   const { data: postList = [], isError, isLoading } = useGetPostList();
+
   if (isLoading) {
-    return <div>..불러오는 중..</div>;
+    return <div>...불러오는 중...</div>;
   }
+
   if (postList.length === 0 || isError) {
     return <NoPostList />;
   }
@@ -14,7 +16,7 @@ const Home = () => {
   return (
     <div>
       {postList.map(item => (
-        <PostListItem key={item.id} id={`${item.id}`} title={item.title} contents={item.contents} tag={item.tag} />
+        <PostListItem key={item.id} {...item} />
       ))}
     </div>
   );
